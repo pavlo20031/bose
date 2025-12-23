cbe98cd8cf597f7a30a08875e62f5ab25437ab5e
pavlo20031 <lesivpavlo999@gmail.com>
2025-12-23 16:29:02 +0200

    Revert "add anim"

--- src/main.js
+++ src/main.js
@@ -6,13 +6,11 @@ let p = document.getElementById('b1');
 let pp = document.getElementById('b2');
 let pic = document.getElementById('p1');
 let picc = document.getElementById('p2');
-let moded = 'black';
 picc.style.display = 'none';
-b.addEventListener('click', () => changeImg('black',0));
-w.addEventListener('click', () => changeImg('grey',0))
-function changeImg(mode,mode2) {
+b.addEventListener('click', () => changeImg('black'));
+w.addEventListener('click', () => changeImg('grey'))
+function changeImg(mode) {
   if (mode === 'black') {
-    moded = 'black';
     console.log('b');
     pic.style.display = 'block';
     picc.style.display = 'none';
@@ -20,76 +18,16 @@ function changeImg(mode,mode2) {
     p.style.border = '2px solid #ff0000';
   }
   if (mode === "grey") {
-    moded = 'grey';
     console.log('g');
     picc.style.display = 'block';
     pic.style.display = 'none';
     p.style.border = 'none';
     pp.style.border = '2px solid #ff0000';
-  }
-  if (mode2 === 0) {
-  pic.animate(
-    [
-      { transform: 'translateY(20px) rotate(0deg)', opacity: 0 },
-      { transform: 'translateY(0) rotate(360deg)', opacity: 1 }
-    ],
-    {
-      duration: 600,
-      easing: 'ease',
-    }
-  );
-  picc.animate(
-    [
-      { transform: 'translateY(20px) rotate(0deg)', opacity: 0 },
-      { transform: 'translateY(0) rotate(360deg)', opacity: 1 }
-    ],
-    {
-      duration: 600,
-      easing: 'ease',
-    }
-  );
-  }
-}
+  }}
 //a
-function imgg (mode){
-  if (mode === 'black') {
-    console.log('b');
-    pic.style.display = 'block';
-    picc.style.display = 'none';
-    pp.style.border = 'none';
-    p.style.border = '2px solid #ff0000';
-    pic.style.opacity = "0.5";
-  }
-  if (mode === "grey") {
-    console.log('g');
-    picc.style.display = 'block';
-    pic.style.display = 'none';
-    p.style.border = 'none';
-    pp.style.border = '2px solid #ff0000';
-    picc.style.opacity = "0.5";
-  }
-  if (mode === "none") {
-    picc.style.opacity = "1";
-    pic.style.opacity = "1";
-  }
-}
-b.addEventListener('mouseenter', () => {
-  imgg('black');
-});
 
-b.addEventListener('mouseleave', () => {
-  imgg('none');
-  changeImg(moded,1)
-});
 
-w.addEventListener('mouseenter', () => {
-  imgg('grey');
-});
 
-w.addEventListener('mouseleave', () => {
-  imgg('none');
-  changeImg(moded,1)
-});
 
 (() => {
   const refs = {
